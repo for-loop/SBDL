2651cd46207e3a38e014fb5b6510503ce3b4ab6d
Hiro Watari <watari.codes@gmail.com>
2024-08-04 16:45:00 -0700

    Add test case for loading spark.conf

--- lib/test_config_loader.py
+++ lib/test_config_loader.py
@@ -14,3 +14,8 @@ def spark():
 def test_sbdl_config_is_read_correctly_for_local(spark):
     conf = ConfigLoader.get_config(LOCAL_ENV)
     assert conf["kafka.topic"] == "sbdl_kafka_cloud"
+
+
+def test_spark_config_is_read_correctly_for_local():
+    spark_conf = ConfigLoader.get_spark_conf(LOCAL_ENV)
+    assert spark_conf.get("spark.app.name") == "sbdl-local"
