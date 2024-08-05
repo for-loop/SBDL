0773a169a3d04ac7d47930db8fbc55dce817bdda
Hiro Watari <watari.codes@gmail.com>
2024-08-05 16:44:31 -0700

    Move Factory class out of the Extractor file

--- lib/Extractor.py
+++ lib/Extractor.py
@@ -26,15 +26,3 @@ class CsvExtractor(Extractor):
             .option("inferSchema", "true")
             .load(path)
         )
-
-
-class Factory:
-
-    def __init__(self, conf):
-        self.conf = conf
-
-    def make_extractor(self, spark):
-        if self.conf["enable.hive"] == "true":
-            return HiveExtractor(spark)
-
-        return CsvExtractor(spark)
