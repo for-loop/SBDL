4915c4e98405810a3c4ee8e2c07969ad3fe3a905
Hiro Watari <watari.codes@gmail.com>
2024-08-05 13:49:05 -0700

    Add test for Hive table

--- lib/DataLoader.py
+++ lib/DataLoader.py
@@ -3,14 +3,17 @@ from pyspark.sql import DataFrame
 
 class DataLoader:
 
-    def __init__(self, spark):
+    def __init__(self, spark, conf):
         self.spark = spark
+        self.conf = conf
 
-    def load(self, path) -> DataFrame:
-        df = (
+    def load(self, path_or_table) -> DataFrame:
+        if self.conf["enable.hive"] == "true":
+            return self.spark.sql(f"SELECT * FROM {path_or_table}")
+
+        return (
             self.spark.read.format("csv")
             .option("header", "true")
             .option("inferSchema", "true")
-            .load(path)
+            .load(path_or_table)
         )
-        return df
