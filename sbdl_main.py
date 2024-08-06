90a8046587e1beabe2de8a3d17af1b469e2857f7
Hiro Watari <watari.codes@gmail.com>
2024-08-06 11:53:57 -0700

    Extract source data in main

--- sbdl_main.py
+++ sbdl_main.py
@@ -2,6 +2,9 @@ import sys
 
 from lib import Utils
 from lib.logger import Log4j
+from lib.ConfigLoader import get_config
+from lib.ExtractorFactoryImpl import ExtractorFactoryImpl
+from lib.Extractor import Extractor
 
 if __name__ == "__main__":
 
@@ -15,4 +18,19 @@ if __name__ == "__main__":
     spark = Utils.get_spark_session(job_run_env)
     logger = Log4j(spark)
 
-    logger.info("Finished creating Spark Session")
+    logger.info("Created Spark Session")
+
+    conf = get_config(job_run_env)
+    ef = ExtractorFactoryImpl(conf)
+    e = ef.make_extractor(spark)
+
+    logger.info("Extracting Source data")
+
+    df_accounts = e.extract(conf["accounts.data.location"])
+    df_parties = e.extract(conf["party.data.location"])
+    df_party_address = e.extract(conf["address.data.location"])
+
+    logger.info("Extracted Source data")
+
+    spark.stop()
+    logger.info("Stopped Spark Session")
