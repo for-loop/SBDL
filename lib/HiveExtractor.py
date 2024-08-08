ed3b2a601c24530e885cf24f1f404cd554bad7b1
Hiro Watari <watari.codes@gmail.com>
2024-08-08 16:25:43 -0700

    Refactor and remove exception handling from extract method

--- lib/HiveExtractor.py
+++ lib/HiveExtractor.py
@@ -1,13 +1,9 @@
 from pyspark.sql import DataFrame
 from lib.Extractor import Extractor
 from lib.EntitiesConfig import EntitiesConfig
-from lib.MissingSourceLocationError import MissingSourceLocationError
 
 
 class HiveExtractor(Extractor):
 
     def extract(self, entity_config: EntitiesConfig) -> DataFrame:
-        if len(entity_config.source_location) == 0:
-            raise MissingSourceLocationError("Source location is required in sbdl.conf")
-
         return self.spark.sql(f"SELECT * FROM {entity_config.source_location}")
