3cb988052eb84c9dc59403f934dad04bcfeacddc
Hiro Watari <watari.codes@gmail.com>
2024-08-13 14:34:05 -0700

    Add join method to PartiesTransformer

--- tests/test_parties_transformer.py
+++ tests/test_parties_transformer.py
@@ -11,6 +11,13 @@ from transform.PartiesTransformer import PartiesTransformer
 
 from schema.PartiesSchema import PartiesSchema
 from schema.TransformedPartiesSchema import TransformedPartiesSchema
+from schema.TransformedAddressesSchema import TransformedAddressesSchema
+from schema.TransformedPartyAddressesSchema import TransformedPartyAddressesSchema
+
+from tests.test_addresses_transformer import (
+    TRANSFORMED_ROW_WITH_ADDRESS1,
+    TRANSFORMED_ROW_WITH_ADDRESS2,
+)
 
 
 @pytest.fixture(scope="session")
@@ -28,7 +35,7 @@ ROW_WITH_PARTY1 = Row(
 
 ROW_WITH_PARTY2 = Row(
     datetime.fromisoformat("2022-08-02"),
-    "6982391061",
+    "6982391060",
     "9823462811",
     "F-N",
     datetime.fromisoformat("2018-08-31T05:27:22.000+05:30"),
@@ -47,7 +54,7 @@ TRANSFORMED_ROW_WITH_PARTY1 = Row(
 )
 
 TRANSFORMED_ROW_WITH_PARTY2 = Row(
-    "6982391061",
+    "6982391060",
     "9823462811",
     {"operation": "INSERT", "newValue": "9823462811", "oldValue": None},
     {"operation": "INSERT", "newValue": "F-N", "oldValue": None},
@@ -56,6 +63,105 @@ TRANSFORMED_ROW_WITH_PARTY2 = Row(
         "newValue": datetime.fromisoformat("2018-08-31T05:27:22.000+05:30"),
         "oldValue": None,
     },
+)
+
+TRANSFORMED_ROW_WITH_PARTY_ADDRESS1 = Row(
+    "6982391060",
+    [
+        {
+            "partyIdentifier": {
+                "operation": "INSERT",
+                "newValue": "9823462810",
+                "oldValue": None,
+            },
+            "partyRelationshipType": {
+                "operation": "INSERT",
+                "newValue": "F-N",
+                "oldValue": None,
+            },
+            "partyRelationStartDateTime": {
+                "operation": "INSERT",
+                "newValue": datetime.fromisoformat("2019-07-29T06:21:32.000+05:30"),
+                "oldValue": None,
+            },
+            "partyAddress": {
+                "operation": "INSERT",
+                "newValue": {
+                    "addressLine1": "45229 Drake Route",
+                    "addressLine2": "13306 Corey Point",
+                    "addressCity": "Shanefort",
+                    "addressPostalCode": "77163",
+                    "addressCountry": "Canada",
+                    "addressStartDate": datetime.fromisoformat("2019-02-26"),
+                },
+                "oldValue": None,
+            },
+        }
+    ],
+)
+
+TRANSFORMED_ROW_WITH_PARTY_ADDRESSES_OF_COMMON_ACCOUNT_ID = Row(
+    "6982391060",
+    [
+        {
+            "partyIdentifier": {
+                "operation": "INSERT",
+                "newValue": "9823462810",
+                "oldValue": None,
+            },
+            "partyRelationshipType": {
+                "operation": "INSERT",
+                "newValue": "F-N",
+                "oldValue": None,
+            },
+            "partyRelationStartDateTime": {
+                "operation": "INSERT",
+                "newValue": datetime.fromisoformat("2019-07-29T06:21:32.000+05:30"),
+                "oldValue": None,
+            },
+            "partyAddress": {
+                "operation": "INSERT",
+                "newValue": {
+                    "addressLine1": "45229 Drake Route",
+                    "addressLine2": "13306 Corey Point",
+                    "addressCity": "Shanefort",
+                    "addressPostalCode": "77163",
+                    "addressCountry": "Canada",
+                    "addressStartDate": datetime.fromisoformat("2019-02-26"),
+                },
+                "oldValue": None,
+            },
+        },
+        {
+            "partyIdentifier": {
+                "operation": "INSERT",
+                "newValue": "9823462811",
+                "oldValue": None,
+            },
+            "partyRelationshipType": {
+                "operation": "INSERT",
+                "newValue": "F-N",
+                "oldValue": None,
+            },
+            "partyRelationStartDateTime": {
+                "operation": "INSERT",
+                "newValue": datetime.fromisoformat("2018-08-31T05:27:22.000+05:30"),
+                "oldValue": None,
+            },
+            "partyAddress": {
+                "operation": "INSERT",
+                "newValue": {
+                    "addressLine1": "361 Robinson Green",
+                    "addressLine2": "3511 Rebecca Mission",
+                    "addressCity": "North Tyler",
+                    "addressPostalCode": "34118",
+                    "addressCountry": "Canada",
+                    "addressStartDate": datetime.fromisoformat("2018-01-28"),
+                },
+                "oldValue": None,
+            },
+        },
+    ],
 )
 
 
@@ -95,5 +201,49 @@ def test_transform_dataframe_with_two_rows(spark):
 
     t = PartiesTransformer(spark)
     actual = t.transform(df)
+
+    assert actual.collect() == expected.collect()
+
+
+def test_join_one_transformed_address(spark):
+
+    expected = spark.createDataFrame(
+        [TRANSFORMED_ROW_WITH_PARTY_ADDRESS1],
+        schema=TransformedPartyAddressesSchema.get_schema(),
+    ).repartition(2)
+
+    transformed_parties = spark.createDataFrame(
+        [TRANSFORMED_ROW_WITH_PARTY1], schema=TransformedPartiesSchema.get_schema()
+    ).repartition(2)
+
+    transformed_addresses = spark.createDataFrame(
+        [TRANSFORMED_ROW_WITH_ADDRESS1], schema=TransformedAddressesSchema.get_schema()
+    ).repartition(2)
+
+    t = PartiesTransformer(spark)
+    actual = t.join(transformed_parties, transformed_addresses)
+
+    assert actual.collect() == expected.collect()
+
+
+def test_join_two_transformed_addresses_with_common_account_id(spark):
+
+    expected = spark.createDataFrame(
+        [TRANSFORMED_ROW_WITH_PARTY_ADDRESSES_OF_COMMON_ACCOUNT_ID],
+        schema=TransformedPartyAddressesSchema.get_schema(),
+    ).repartition(2)
+
+    transformed_parties = spark.createDataFrame(
+        [TRANSFORMED_ROW_WITH_PARTY1, TRANSFORMED_ROW_WITH_PARTY2],
+        schema=TransformedPartiesSchema.get_schema(),
+    ).repartition(2)
+
+    transformed_addresses = spark.createDataFrame(
+        [TRANSFORMED_ROW_WITH_ADDRESS1, TRANSFORMED_ROW_WITH_ADDRESS2],
+        schema=TransformedAddressesSchema.get_schema(),
+    ).repartition(2)
+
+    t = PartiesTransformer(spark)
+    actual = t.join(transformed_parties, transformed_addresses)
 
     assert actual.collect() == expected.collect()
--- transform/PartiesTransformer.py
+++ transform/PartiesTransformer.py
@@ -1,5 +1,5 @@
 from pyspark.sql.dataframe import DataFrame
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, collect_list, struct
 
 from transform.Transformer import Transformer
 
@@ -13,4 +13,20 @@ class PartiesTransformer(Transformer):
             self._add_insert(col("party_id"), "partyIdentifier"),
             self._add_insert(col("relation_type"), "partyRelationshipType"),
             self._add_insert(col("relation_start_date"), "partyRelationStartDateTime"),
+        )
+
+    def join(self, party_df: DataFrame, address_df: DataFrame) -> DataFrame:
+        return (
+            party_df.join(address_df, "party_id", "left_outer")
+            .groupBy("account_id")
+            .agg(
+                collect_list(
+                    struct(
+                        "partyIdentifier",
+                        "partyRelationshipType",
+                        "partyRelationStartDateTime",
+                        "partyAddress",
+                    ).alias("partyDetails")
+                ).alias("partyRelations")
+            )
         )
