a3e00ed4ce4cd072f68de3d0556dbcdf7a1f1648
Hiro Watari <watari.codes@gmail.com>
2024-08-13 20:26:18 -0700

    Fix linter issues

--- tests/test_transformer_facade.py
+++ tests/test_transformer_facade.py
@@ -37,10 +37,25 @@ def test_transform_in_transformer_facade(spark):
 
 def test_transform_all_test_data_returns_eight_rows(spark):
 
-    accounts_df = spark.read.format("csv").option("header", "true").schema(AccountsSchema.get_schema()).load("test_data/accounts/")
-    parties_df = spark.read.format("csv").option("header", "true").schema(PartiesSchema.get_schema()).load("test_data/parties/")
-    addresses_df = spark.read.format("csv").option("header", "true").schema(AddressesSchema.get_schema()).load("test_data/party_address/")
-    
+    accounts_df = (
+        spark.read.format("csv")
+        .option("header", "true")
+        .schema(AccountsSchema.get_schema())
+        .load("test_data/accounts/")
+    )
+    parties_df = (
+        spark.read.format("csv")
+        .option("header", "true")
+        .schema(PartiesSchema.get_schema())
+        .load("test_data/parties/")
+    )
+    addresses_df = (
+        spark.read.format("csv")
+        .option("header", "true")
+        .schema(AddressesSchema.get_schema())
+        .load("test_data/party_address/")
+    )
+
     tf = TransformerFacade()
     df = tf.transform(accounts_df, parties_df, addresses_df)
 
